54782e24762d83d6b401b5128930e6acd1fa4c89
azure <rz7@outlook.jp>
2019-07-15 13:57:57 +0900

    Change Message: replace Can't by Couldn't

--- mTwit/Error.py
+++ mTwit/Error.py
@@ -32,7 +32,7 @@ class VerifyError(ErrorNotification):
 
 
 class TaskbarError(ErrorNotification):
-    def __init__(self, reason="Can't get Taskbar Position"):
+    def __init__(self, reason="Couldn't get Taskbar Position"):
         Exception.__init__(self, reason)
         super().__init__(reason)
         pass
